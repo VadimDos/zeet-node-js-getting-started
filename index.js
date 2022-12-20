46e5bb93e036b31ace66e7be171f7480b4b322ce
vadim <vadim@appla.cy>
2022-12-20 16:47:10 +0300

    info

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const nodejsinfo = require('nodejs-info');
+const nodeinfo = require('node-info');
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/nodejsinfo', (req, res) => res.send(nodejsinfo(req)))
+  .get('/nodeinfo', (req, res) => res.send(nodeinfo(req)))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
